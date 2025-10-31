46eefb62392dbe01a12589cafbd5d22fba767a18
Gabriel99 <155887339+Gabrieln99@users.noreply.github.com>
2025-10-31 09:01:21 +0100

    dodana prezentacija md

--- rs1_vjezba/vjezba1.py
+++ rs1_vjezba/vjezba1.py
@@ -8,4 +8,85 @@ print(a) # Hello, World!
 print("Hello world")
 
 """
+"""
+a = 5 
+b = 10 
+
+print(a + b)  # Zbrajanje
+print(a - b)  # Oduzimanje  
+print(a * b)  # Množenje
+print(a / b)  # Dijeljenje
+print(a // b) # Cjelobrojno dijeljenje
+print(a % b)  # Modulo
+print(a ** b) # Stepenovanje
+
+"""
+
+"""
+
+print("ovo je moj prvi kalkusulator u pythonu")
+
+
+print("Unesite prvi broj: ")
+a = int(input())
+
+print("Unesite drugi broj: ")
+b = int(input())
+
+print("Unesite operator:")
+operator = input()
+
+match operator:
+    case "+":
+        print(a+b)
+    case "-":
+        print(a-b)
+    case "*":
+        print(a*b)
+    case "/":
+        print(a/b)
+    case _ : 
+        print("unjeli ste krivu oznaku, promjenite ju!")
+
+"""
+"""
+
+print("program za provjeravanje prjestupne godine")
+
+print("unesite godinu:")
+a = int(input())
+
+if (a % 4 == 0 and a % 100 != 0 ) or (a % 400 == 0):
+    print("godina ",  a , " je pristupna" )
+else : 
+    print("godina", a , "nije prijestupna ")
+
+
+"""
+
+import random
+
+print("pogodite broj o d 1 do 100: ")
+
+print("unesite jedan broj: ")
+
+a = int(input())
+
+broj = random.randint(1,100)
+
+while a != broj:
+    print("ponovno unesite broj vas broj nije tocan")
+
+    if (a < broj):
+        print("unjeli ste previse mali broj ")
+    elif (a > broj):
+        print("Unjeli set pre veliki broj ")
+
+    a = int(input())
+
+print("pogodili ste broj")
+
+
+
+
 
